1e97819228feab919a842b45c072f5646694f691
W_ngyx <08153531@cumt.edu.cn>
2019-12-01 19:36:36 +0800

    初始化

---
+++ src/main/java/com/w_ngyx/cateye/film/CateyeFilmApplication.java
@@ -0,0 +1,13 @@
+package com.w_ngyx.cateye.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateyeFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateyeFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w_ngyx/cateye/film/CateyeFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_ngyx.cateye.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateyeFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
